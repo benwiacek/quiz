35cadc4b923d1994cc40db5ae328278438e20a36
Wiacek <benjamin.wiacek@gmail.com>
2026-01-28 14:58:07 -0500

    Fix assets and font paths for production build

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import Homepage from "./components/Homepage"
 import Questions from "./components/Questions"
+import logo from "./assets/images/ifaw_logo_RGB.jpg"
+import landscapeImg from "./assets/images/landscape.jpg"
 
 export default function App() {
 
@@ -10,11 +12,11 @@ export default function App() {
     return (
 		<div className="page">
             <header>
-                <a href="https://www.ifaw.org" target="_blank" rel="noopener noreferrer"><img className="logo" src="./src/assets/ifaw_logo_RGB.jpg" alt="IFAW logo"/></a>
+                <a href="https://www.ifaw.org" target="_blank" rel="noopener noreferrer"><img className="logo" src={logo} alt="IFAW logo"/></a>
                 <a href="https://www.ifaw.org/?form=join" className="brand-btn" target="_blank" rel="noopener noreferrer">Donate</a>
             </header>
             <main>
-                <img src="./src/assets/landscape.jpg" className="background-img" alt="Landscape shot of Amboseli National Park." />
+                <img src={landscapeImg} className="background-img" alt="Landscape shot of Amboseli National Park." />
                 {!isGameStarted? 
                     <Homepage 
                         setIsGameStarted = {setIsGameStarted}
